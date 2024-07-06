d94aa242486fe950b85cada7ddb7a95122971a97
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-06 23:23:14 +0300

    feat: add localStorage logic

--- my-react-app/src/components/SearchBlock/SearchBlock.tsx
+++ my-react-app/src/components/SearchBlock/SearchBlock.tsx
@@ -1,15 +1,33 @@
-import React, { FormEventHandler } from "react";
-import { FormFieldsType, SearchBlockPropsType } from "./searchBlock.types";
+import React, { ChangeEventHandler, FormEventHandler } from "react";
+import {
+  FormFieldsType,
+  SearchBlockPropsType,
+  SearchBlockStateType,
+} from "./searchBlock.types";
 
-export class SearchBlock extends React.Component<SearchBlockPropsType> {
+export class SearchBlock extends React.Component<
+  SearchBlockPropsType,
+  SearchBlockStateType
+> {
   constructor(props: SearchBlockPropsType) {
     super(props);
+    const initialValue = window.localStorage.getItem("searchValue") || "";
+    this.state = {
+      searchValue: initialValue,
+    };
+    this.props.searchDataHandler(initialValue);
   }
-  
+
   submitHandler: FormEventHandler = (e) => {
     e.preventDefault();
     const newValue = (e.target as FormFieldsType).elements.searchValue.value;
     this.props.searchDataHandler(newValue);
+    window.localStorage.setItem("searchValue", newValue);
+  };
+
+  changeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const newValue = e.target.value;
+    this.setState({ searchValue: newValue });
   };
 
   render(): React.ReactNode {
@@ -23,6 +41,8 @@ export class SearchBlock extends React.Component<SearchBlockPropsType> {
               type="text"
               name="searchValue"
               id="searchValue"
+              value={this.state.searchValue}
+              onChange={this.changeHandler}
             />
           </label>
           <input className="search-block-button" type="submit" value="Search" />
--- my-react-app/src/components/SearchBlock/searchBlock.types.ts
+++ my-react-app/src/components/SearchBlock/searchBlock.types.ts
@@ -1,5 +1,10 @@
 export type SearchBlockPropsType = {
   searchDataHandler: (newString: string) => void;
 };
+export type SearchBlockStateType = {
+  searchValue: string;
+};
 
-export type FormFieldsType = EventTarget & { elements: { searchValue: {value: string} } };
+export type FormFieldsType = EventTarget & {
+  elements: { searchValue: { value: string } };
+};
