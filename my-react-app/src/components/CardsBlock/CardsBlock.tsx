77191756b37d3265142effdf52f70bd9d5b921eb
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-07 13:09:05 +0300

    feat: test

--- my-react-app/src/components/CardsBlock/CardsBlock.tsx
+++ my-react-app/src/components/CardsBlock/CardsBlock.tsx
@@ -5,8 +5,17 @@ export class CardsBlock extends React.Component<CardsBlockPropsType> {
   render(): React.ReactNode {
     return (
       <div className="cards-block-wrap">
-        {this.props.data?.map((item) => (
-          <p key={item.uid}>{JSON.stringify(item)}</p>
+        {this.props.hhhdata?.map((item) => (
+          <div className="card-container" key={item.uid}>
+            <div className="card-name">Name: {JSON.stringify(item.name)}</div>
+            <div className="card-description">
+              Small description:
+              <p>earthAnimal: {JSON.stringify(item.earthAnimal)}</p>
+              <p>avian: {JSON.stringify(item.avian)}</p>
+              <p>canine: {JSON.stringify(item.canine)}</p>
+              <p>feline: {JSON.stringify(item.feline)}</p>
+            </div>
+          </div>
         ))}
       </div>
     );
