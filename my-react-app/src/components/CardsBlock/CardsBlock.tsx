2b3f0ccb3bed3fa29df100fa4fefaa5e94a70bb8
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-07 21:46:16 +0300

    feat: test husky

--- my-react-app/src/components/CardsBlock/CardsBlock.tsx
+++ my-react-app/src/components/CardsBlock/CardsBlock.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CardsBlockPropsType } from "./cardsBlock.types";
 
 export class CardsBlock extends React.Component<CardsBlockPropsType> {
+  xcxc
   render(): React.ReactNode {
     return (
       <div className="cards-block-wrap">
