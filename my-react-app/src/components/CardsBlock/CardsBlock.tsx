fad6fdae638105d391da9ef07371b315a0dc5cbd
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-08 22:55:42 +0300

    fix: fix husky

--- my-react-app/src/components/CardsBlock/CardsBlock.tsx
+++ my-react-app/src/components/CardsBlock/CardsBlock.tsx
@@ -4,7 +4,7 @@ import { Loader } from "../Loader/Loader";
 
 export class CardsBlock extends React.Component<CardsBlockPropsType> {
   render(): React.ReactNode {
-    const a = 5;
+    const a = 6;
     return (
       <div className="cards-block-wrap">
         {this.props.isFetching ? (
