d8bb1930a954be75431b67b61d6b486a0655fddb
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-12 16:59:46 +0300

    refactor: refactor cards block

--- my-react-app/src/components/CardsBlock/CardsBlock.tsx
+++ my-react-app/src/components/CardsBlock/CardsBlock.tsx
@@ -1,28 +1,26 @@
-import React from "react";
-import { CardsBlockPropsType } from "./cardsBlock.types";
 import { Loader } from "../Loader/Loader";
+import { CardsBlockPropsType } from "./cardsBlock.types";
 
-export class CardsBlock extends React.Component<CardsBlockPropsType> {
-  render(): React.ReactNode {
-    return (
-      <div className="cards-block-wrap">
-        {this.props.isFetching ? (
-          <Loader />
-        ) : (
-          this.props.data?.map((item) => (
-            <div className="card-container" key={item.uid}>
-              <div className="card-name">Name: {JSON.stringify(item.name)}</div>
-              <div className="card-description">
-                Small description:
-                <p>earthAnimal: {item.earthAnimal ? "yes" : "no"}</p>
-                <p>avian: {item.avian ? "yes" : "no"}</p>
-                <p>canine: {item.canine ? "yes" : "no"}</p>
-                <p>feline: {item.feline ? "yes" : "no"}</p>
-              </div>
+export const CardsBlock = ({ isFetching, data }: CardsBlockPropsType) => {
+  return (
+    <div className="cards-block-wrap">
+      {isFetching ? (
+        <Loader />
+      ) : (
+        data?.map((item) => (
+          <div className="card-container" key={item.uid}>
+            <div className="card-name">Name: {JSON.stringify(item.name)}</div>
+            <div className="card-description">
+              Small description:
+              <p>earthAnimal: {item.earthAnimal ? "yes" : "no"}</p>
+              <p>avian: {item.avian ? "yes" : "no"}</p>
+              <p>canine: {item.canine ? "yes" : "no"}</p>
+              <p>feline: {item.feline ? "yes" : "no"}</p>
             </div>
-          ))
-        )}
-      </div>
-    );
-  }
-}
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
