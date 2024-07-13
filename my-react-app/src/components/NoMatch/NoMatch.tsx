61308f95bc9076c3e2a903fe180537c32b0c3501
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-13 15:30:12 +0300

    feat: add no match page

--- my-react-app/src/components/NoMatch/NoMatch.tsx
+++ my-react-app/src/components/NoMatch/NoMatch.tsx
@@ -1,3 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import s from './noMatch.module.css'
+
 export const NoMatch = () => {
-  return <div>NoMatch</div>;
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className={s.notFound}>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <button onClick={goHome}>Go to Home</button>
+    </div>
+  );
 };
