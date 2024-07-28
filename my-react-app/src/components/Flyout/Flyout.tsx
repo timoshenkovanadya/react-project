28b915a8177cc9b04076fb9115a3191ae1b07272
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-07-28 18:53:58 +0300

    feat: add unselect

--- my-react-app/src/components/Flyout/Flyout.tsx
+++ my-react-app/src/components/Flyout/Flyout.tsx
@@ -1,19 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import s from "./flyout.module.css";
-import { adapterSelectors, RootState } from "../../store/store";
-import { useMemo } from "react";
+import { adapterSelectors, RootState, toggleSelected } from "../../store/store";
+import { useCallback, useMemo } from "react";
 
 const Flyout = () => {
+  const dispatch = useDispatch();
   const animals = useSelector((store: RootState) =>
     adapterSelectors.selectAll(store),
   );
   const count = useMemo(() => animals?.length, [animals]);
+  const unselectHandler = useCallback(() => {
+    animals.forEach((animal) => dispatch(toggleSelected(animal)));
+  }, [animals]);
 
   if (!count) return null;
   return (
     <div className={s.wrap}>
       <div className={s.label}>{count} items are selected</div>
-      <button className={s.button}>Unselect all</button>
+      <button className={s.button} onClick={unselectHandler}>
+        Unselect all
+      </button>
       <button className={s.button}>Download</button>
     </div>
   );
